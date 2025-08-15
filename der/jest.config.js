021e597900be4a7b3f9888f22328fb5186b9b3d1
Maurice Lam <me@mauricelam.com>
2025-08-14 22:37:02 -0700

    Add der test

---
+++ der/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    "\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js"
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          jsx: 'react',
+          esModuleInterop: true,
+        },
+      },
+    ],
+  },
+};
