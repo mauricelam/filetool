f3917db3ec61f708b9ab7a1cddf75e34ffef88c3
Maurice Lam <me@mauricelam.com>
2026-04-02 22:21:58 -0700

    Fix tests

--- der/jest.config.js
+++ der/jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
-    testEnvironment: 'jsdom',
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
     "\.(css|less|sass|scss)$": "identity-obj-proxy",
     "\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js"
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/tests/'],
   transform: {
     '^.+\.tsx?$': [
       'ts-jest',
